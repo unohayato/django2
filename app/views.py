043e73d1503fa4f52038c0fc9c4070f2b7a6ba49
unohayato <mamidori1p1p@gmail.com>
2022-08-01 22:48:41 +0900

    for

--- app/views.py
+++ app/views.py
@@ -5,7 +5,21 @@ class IndexView(TemplateView):
 
   def get_context_data(self):
     ctxt = super().get_context_data()
+    ctxt['username'] = 'u'
     return ctxt
 
 class AboutView(TemplateView):
   template_name = 'app/about.html'
+  def get_context_data(self):
+    ctxt = super().get_context_data()
+    ctxt['num_services'] = 123456789
+    ctxt['skills'] = [
+      'Python',
+      'Javascript',
+      'Rust',
+      'PHP',
+      'C++'
+    ]
+    return ctxt
+  
+  
