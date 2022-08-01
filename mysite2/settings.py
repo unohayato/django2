1136a53080118482db962f641fc0c297a2bec223
unohayato <mamidori1p1p@gmail.com>
2022-08-01 21:59:41 +0900

    index,about Veiw

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5y^#g@b0vs$q%1&e*!7ocq!)7**4g1lo#)2h(6-2+h+u64c(k='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
