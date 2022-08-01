b7ed80a0e1aa7aa47db1c20e3360edfc8c13c78d
unohayato <mamidori1p1p@gmail.com>
2022-08-01 22:32:48 +0900

    変数

--- app/views.py
+++ app/views.py
@@ -3,5 +3,9 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
   template_name = 'app/index.html'
 
+  def get_context_data(self):
+    ctxt = super().get_context_data()
+    return ctxt
+
 class AboutView(TemplateView):
   template_name = 'app/about.html'
